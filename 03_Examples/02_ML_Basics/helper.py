04ee27366e0036f96be0041392f522e40cb0dbf6
kacper <kacper.lukawski@uj.edu.pl>
2016-12-05 00:10:51 +0100

    Changed the layers in face recognition example.

--- 03_Examples/02_ML_Basics/helper.py
+++ 03_Examples/02_ML_Basics/helper.py
@@ -5,10 +5,10 @@ def get_random_sample():
     """
     Creates a random sample for the purposes of perceptron example.
     This example expects to belong to class "1" if it is above the
-    line created by a function: f(x) = -x + 1.5 and to "0" if it lies
+    line created by a function: f(x) = -x + 1.5 and to "-1" if it lies
     below or on it. Each value of created vector is chosen from
     the [0, 1) interval.
     :return:
     """
     x, y = random.random(), random.random()
-    return (x, y), int(y > -x + 1.5)
+    return (x, y), 1.0 if y > -x + 1.5 else -1.0
